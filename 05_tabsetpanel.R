7ea7c04c18f0756d7d5b90f39e47ef741633ab16
Athos Petri Damiani <athos.damiani@gmail.com>
2019-11-19 02:07:26 -0300

    ajustes

--- 05_tabsetpanel.R
+++ 05_tabsetpanel.R
@@ -4,12 +4,12 @@ ui <- fluidPage(
   tabsetPanel(
     tabPanel(
       "tab 1",
-      sliderInput("num1", "NUM", 0, 100),
+      sliderInput("num1", "NUM 1", 0, 100, value = 50),
       plotOutput("rnorm")
     ),
     tabPanel(
       title = "tab 2",
-      sliderInput("num2", "NUM", 0, 100),
+      sliderInput("num2", "NUM 2", 0, 100, value = 20),
       plotOutput("runif")
     )
   )
